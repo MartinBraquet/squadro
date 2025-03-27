ff42839c7b157fa5fb8d2e4ad7179108112857dc
MartinBraquet <martin.braquet@gmail.com>
2025-03-27 15:50:45 +0700

    Clean human_agent handling

--- squadro/human_agent.py
+++ squadro/human_agent.py
@@ -1,38 +1,24 @@
 import pygame
 
 from squadro.agent import Agent
-from squadro.board import get_position_from_click
+from squadro.board import get_position_from_click, check_quit
 
 
 class MyAgent(Agent):
 
-	def get_action(self, state, last_action, time_left):
-		played = False
-		action = None
-		while not played:
-			for event in pygame.event.get():
-				if event.type == pygame.MOUSEBUTTONUP:
-					pos = get_position_from_click()
-					valid_click = False
+    def get_action(self, state, last_action, time_left):
+        while True:
+            for event in pygame.event.get():
+                check_quit(event)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    click_pos = get_position_from_click()
+                    player = state.get_cur_player()
+                    pawn = click_pos[player] - 1
+                    if 0 <= pawn <= state.n_pawns - 1:
+                        pawn_pos = state.get_pawn_position(player, pawn)
+                        if pawn_pos == click_pos:
+                            if state.is_action_valid(pawn):
+                                return pawn
 
-					if state.get_cur_player() == 0:
-						pawn = pos[0] - 1
-						if 0 <= pawn <= state.n_pawns - 1 and state.get_pawn_position(0, pawn)[1] <= pos[1] <= state.get_pawn_position(0, pawn)[1] + 1:
-							valid_click = True
-					else:
-						pawn = pos[1] - 1
-						if 0 <= pawn <= state.n_pawns - 1 and state.get_pawn_position(1, pawn)[0] <= pos[0] <= state.get_pawn_position(1, pawn)[0] + 1:
-							valid_click = True
-					
-					if valid_click and state.is_action_valid(pawn):
-						played = True
-						action = pawn
-
-				if event.type == pygame.QUIT:
-					pygame.quit() 
-					exit(0)
-
-		return action
-  
-	def get_name(self):
-		return "human_agent"
+    def get_name(self):
+        return "human_agent"
