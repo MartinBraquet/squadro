56911b0d59a62e9f33b4bde6ca90eca515f21ce2
MartinBraquet <martin.braquet@gmail.com>
2025-03-28 17:42:40 +0700

    Add and test new game functionalities and state handling
    Introduced `GameFromState` initialization and added multiple unit tests for timeout handling, state saving/loading, and game-to-dict conversion. Enhanced test coverage ensures functionality for custom states and result persistence is now validated.

--- squadro/tests/test_game.py
+++ squadro/tests/test_game.py
@@ -1,9 +1,15 @@
 import random
+import tempfile
+from pathlib import Path
+from time import sleep
 from unittest import TestCase
+from unittest.mock import patch
 
 import numpy as np
 
-from squadro.game import Game
+from squadro.game import Game, GameFromState
+from squadro.random_agent import MyAgent
+from squadro.squadro_state import SquadroState
 
 
 class TestGame(TestCase):
@@ -26,5 +32,47 @@ class TestGame(TestCase):
 
     def test_game_3_pawns(self):
         game = Game(n_pawns=3, agent_0='random', agent_1='random')
-        action_history = game.run()
-        # print(action_history)
+        game.run()
+
+    def test_game_from_state_initialization(self):
+        state = SquadroState(n_pawns=4, first=1)
+        game = GameFromState(state=state, agent_0="random", agent_1="random")
+        self.assertEqual(game.first, 1)
+        self.assertEqual(game.n_pawns, 4)
+        self.assertEqual(game.winner, None)
+
+    def test_game_from_state_action_history(self):
+        state = SquadroState(n_pawns=3, first=0)
+        game = GameFromState(state=state, agent_0="random", agent_1="random")
+        self.assertListEqual(game.action_history, [])
+
+    @patch.object(MyAgent, "get_action", lambda *a, **kw: sleep(2))
+    def test_time_out(self):
+        game = Game(time_out=1)
+        game.run()
+        self.assertEqual(game.first, game.state.timeout_player)
+
+    def test_time_outs(self):
+        time_out = 10
+        game = Game(time_out=time_out)
+        game.run()
+        self.assertLess(game.times_left[0], time_out)
+        self.assertLess(game.times_left[1], time_out)
+
+    def test_to_dict(self):
+        game = Game(n_pawns=3)
+        self.assertEqual(game.to_dict(), {
+            'action_history': [],
+            'agent_0': 'random',
+            'agent_1': 'random',
+            'state': {'first': 0, 'n_pawns': 3},
+            'winner': None
+        })
+
+    def test_save_and_load_results(self):
+        game = Game(n_pawns=3)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            tmp_file = Path(temp_dir) / "test_results.json"
+            game.save_results(tmp_file)
+            game_loaded = Game.from_file(tmp_file)
+        self.assertEqual(game, game_loaded)
