22c1251614525a427b49bb29885afbf02480740c
MartinBraquet <martin.braquet@gmail.com>
2025-04-16 14:26:40 +0700

    Update MonteCarlo agent tests with adjusted parameters
    Refined test configurations in MonteCarlo agent tests to align with updated game settings. Modified epsilon_move initialization, agent types, pawn numbers, and expected action history to reflect the changes for more accurate testing.

--- squadro/tests/agents/test_montecarlo_agent.py
+++ squadro/tests/agents/test_montecarlo_agent.py
@@ -16,6 +16,7 @@ class TestMonteCarlo(TestCase):
     def test_get_action(self):
         agent = MonteCarloAgent(pid=0)
         agent.max_time = 1e9
+        agent.epsilon_move = 0
 
         state = State(first=0, n_pawns=3)
         for i in range(1, 4):
@@ -24,18 +25,17 @@ class TestMonteCarlo(TestCase):
             self.assertEqual(3 - i, action)
 
     def test_game_ab(self):
-        game = Game(n_pawns=4, agent_0='mcts', agent_1='ab_advancement_deep', first=0)
+        game = Game(n_pawns=4, agent_0='mcts', agent_1='ab_relative_advancement', first=0)
         game.agents[0].mc_steps = 200
         game.run()
         self.assertEqual(game.winner, 0)
 
     def test_game(self):
-        game = Game(n_pawns=4, agent_0='mcts', agent_1='random', first=0)
+        game = Game(n_pawns=3, agent_0='mcts', agent_1='random', first=0)
         game.agents[0].mc_steps = 50
         action_history = game.run()
         self.assertEqual(
-            [1, 0, 3, 0, 3, 0, 0, 1, 0, 3, 0, 2, 0, 0, 0, 1, 0, 2, 1, 0, 1, 1, 1, 3, 1, 1, 1, 2, 2,
-             3, 2, 0, 2, 1, 2, 2, 2],
+            [2, 2, 2, 0, 1, 1, 0, 0, 1, 2, 0, 1, 0, 2, 2, 0, 1, 0, 0, 0, 1, 1, 0, 2, 2, 1, 2],
             action_history
         )
         self.assertEqual(game.winner, 0)
