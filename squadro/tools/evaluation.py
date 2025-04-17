9d8e329b764bad577110bbaf561a4e7179a8c164
MartinBraquet <martin.braquet@gmail.com>
2025-04-17 18:41:28 +0700

    Refactor and document `evaluate_advancement` logic
    Refactored the computation of advancement into a nested function for clarity and added a detailed docstring to explain the purpose and behavior of the method. This improves readability, maintainability, and understanding of the function while preserving its functionality.

--- squadro/tools/evaluation.py
+++ squadro/tools/evaluation.py
@@ -2,18 +2,29 @@ from squadro.state import State
 
 
 def evaluate_advancement(state: State, player_id: int) -> int:
+    """
+    Evaluate the current state (Q value), according to the current player.
+    Normalized between -1 and 1.
+
+    :param state: State to evaluate
+    :param player_id: Player to evaluate
+    :return: Q value
+    """
     max_value = state.n_pawns_to_win * state.max_pos * 2
 
-    if state.game_over():
-        # Needed, otherwise a winning state might be less than a non-winning state.
-        # As a winning state might be as low as 1 (when the opponent is one tile from winning)
-        return 1 if state.winner == player_id else -1
+    def get_summed_advancement():
+        if state.game_over():
+            # Needed, otherwise a winning state might be less than a non-winning state.
+            # As a winning state might be as low as 1 (when the opponent is one tile from winning)
+            return max_value if state.winner == player_id else -max_value
 
-    advancement = state.get_advancement()
-    if player_id == 0:
-        l1, l2 = advancement
-    else:
-        l2, l1 = advancement
-    value = (sum(l1) - min(l1)) - (sum(l2) - min(l2))
+        advancement = state.get_advancement()
+        if player_id == 0:
+            l1, l2 = advancement
+        else:
+            l2, l1 = advancement
+        return (sum(l1) - min(l1)) - (sum(l2) - min(l2))
+
+    value = get_summed_advancement()
     value /= max_value
     return value
