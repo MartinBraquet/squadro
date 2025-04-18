111ec1a57917bd54cbfcdc601bf423f0f30e5d63
MartinBraquet <martin.braquet@gmail.com>
2025-04-18 15:23:32 +0700

    Refine comment in hash_dict for clarity and accuracy.
    The comment in the `hash_dict` function was updated to improve clarity and correct terminology. This enhances code readability and ensures consistent use of language.

--- squadro/tools/serialize.py
+++ squadro/tools/serialize.py
@@ -1,7 +1,7 @@
 def hash_dict(d):
     """Generates a hash for a dictionary, including nested dictionaries."""
-    # May want to use hashlib.sha256(s).hexdigest() in the future for reproducibility and
-    # less risk of collision
+    # May want to use `hashlib.sha256(s).hexdigest()` in the future for reproducibility and
+    # lower risk of collision
     # If the input is a dictionary, convert it to a sorted tuple of key-hash pairs
     if isinstance(d, dict):
         return hash(tuple(sorted((key, hash_dict(value)) for key, value in d.items())))
