6b6906b0ba87894b36e16b2b34bece46c2576d95
MartinBraquet <martin.braquet@gmail.com>
2025-04-18 15:27:57 +0700

    Refactor AdvancementEvaluator and add base Evaluator class
    Moved AdvancementEvaluator to a new evaluator module and implemented it as a subclass of a newly introduced abstract Evaluator base class. This refactor improves modularity and prepares the codebase for future additions of other evaluators like ConstantEvaluator.

---
+++ squadro/evaluators/evaluator.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+from numpy.typing import NDArray
+
+from squadro.state import State
+from squadro.tools.evaluation import evaluate_advancement
+
+
+class Evaluator(ABC):
+    """
+    Base class for state evaluation.
+    """
+
+    @abstractmethod
+    def evaluate(self, state: State) -> tuple[NDArray[np.float64], float]:
+        """
+        Evaluate the current state (Q value and policy), according to the current player.
+
+        Args:
+            state: The current game state to evaluate
+
+        Returns:
+            A tuple containing:
+                - NDArray[np.float64]: Probability distribution over possible actions (policy)
+                - float: Value estimation for the current state from the perspective of the player
+                playing the next move at that state.
+        """
+        ...
+
+
+class AdvancementEvaluator(Evaluator):
+    def evaluate(self, state: State) -> tuple[NDArray[np.float64], float]:
+        p = np.ones(state.n_pawns) / state.n_pawns
+        value = evaluate_advancement(
+            state=state,
+            player_id=state.cur_player,
+        )
+        return p, value
+
+
+class ConstantEvaluator(Evaluator):
+    def __init__(self, constant: float = 0):
+        self.constant = constant
+
+    def evaluate(self, state: State) -> tuple[NDArray[np.float64], float]:
+        p = np.ones(state.n_pawns) / state.n_pawns
+        return p, self.constant
--- squadro/tests/evaluators/test_advancement_evaluator.py
+++ squadro/tests/evaluators/test_advancement_evaluator.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from squadro.agents.montecarlo_agent import AdvancementEvaluator
+from squadro.evaluators.evaluator import AdvancementEvaluator
 from squadro.state import State
 
 
