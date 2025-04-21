71c52bd18c514c716e2161ac18354639ef0dc16c
MartinBraquet <martin.braquet@gmail.com>
2025-04-21 10:29:25 +0700

    Add shortcuts

--- squadro/__init__.py
+++ squadro/__init__.py
@@ -1,3 +1,7 @@
 import os
 
+from squadro.animation.animated_game import RealTimeAnimatedGame, GameAnimation
+from squadro.game import Game
+from squadro.tools.agents import AVAILABLE_AGENTS
+
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
