250239ce733d06ebea3ffb5339bbdeed27a92d6b
MartinBraquet <martin.braquet@gmail.com>
2025-04-21 10:20:10 +0700

    Add get_best_real_time_game_agent

--- squadro/agents/best.py
+++ squadro/agents/best.py
@@ -1,5 +1,11 @@
 from squadro.agents.alphabeta_agent import AlphaBetaAdvancementDeepAgent
+from squadro.tools.constants import DefaultParams
 
 
 def get_best_agent(**kwargs):
     return AlphaBetaAdvancementDeepAgent(**kwargs)
+
+
+def get_best_real_time_game_agent(**kwargs):
+    kwargs.setdefault('max_time_per_move', DefaultParams.max_time_per_move_real_time)
+    return get_best_agent(**kwargs)
