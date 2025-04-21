513ac1abc53ac5b561115a710568db992bec0113
MartinBraquet <martin.braquet@gmail.com>
2025-04-21 19:05:45 +0700

    Add multiprocess training

--- squadro/training/q_learning.py
+++ squadro/training/q_learning.py
@@ -1,3 +1,6 @@
+from contextlib import nullcontext
+from multiprocessing import Process, Manager
+
 from squadro import Game
 from squadro.agents.montecarlo_agent import MonteCarloQLearningAgent
 from squadro.evaluators.evaluator import QLearningEvaluator
@@ -15,7 +18,8 @@ class QLearningTrainer:
         n_steps=None,
         eval_interval=None,
         eval_steps=None,
-        model_path=None
+        model_path=None,
+        parallel=None
     ):
         """
         :param n_pawns: number of pawns in the game.
@@ -25,50 +29,102 @@ class QLearningTrainer:
         :param eval_interval: interval at which to evaluate the agent.
         :param eval_steps: number of steps to evaluate the agent.
         :param model_path: path to save the model.
+        :param parallel: whether to run in parallel on multiple CPUs/GPUs.
         """
         self.n_pawns = n_pawns or DefaultParams.n_pawns
         self.lr = lr or .2
         self.gamma = gamma or .95
         self.n_steps = n_steps or int(5e4)
-        self.eval_interval = eval_interval or int(1e3)
+        self.eval_interval = eval_interval or int(500)
         self.eval_steps = eval_steps or int(100)
+        self.parallel = parallel if parallel is not None else False
 
-        self.agent = 'mcts_q_learning'
         model_path = model_path or DATA_PATH / f"q_table_{self.n_pawns}.json"
-        self.evaluator = QLearningEvaluator(model_path=model_path)
-        self.evaluator_old = QLearningEvaluator(model_path=model_path.replace(".json", "_old.json"))
+        self.agent = MonteCarloQLearningAgent(
+            evaluator=QLearningEvaluator(model_path=model_path),
+            is_training=True,
+        )
+        self.evaluator_old = QLearningEvaluator(
+            model_path=str(model_path).replace(".json", "_old.json")
+        )
+
+    @property
+    def evaluator(self) -> QLearningEvaluator:
+        return self.agent.evaluator  # noqa
 
     def run(self) -> None:
         """
         Run the training loop.
         """
         self.evaluator.dump(self.evaluator_old.model_path)
-
         # Should be close to 50% as the agents are the same
-        logger.info(self.evaluate_agent())
+        # logger.info(self.evaluate_agent())
 
-        for n in range(self.n_steps):
-            game = Game(
-                n_pawns=self.n_pawns,
-                agent_0=self.agent,
-                agent_1=self.agent,
-                save_states=True
-            )
-            game.run()
+        if self.parallel:
+            manager = Manager()
+            q_table = manager.dict()
+            lock = manager.Lock()
 
-            self.back_propagate(game.state_history.copy())
+            processes = [
+                Process(target=self.target, args=(q_table, lock, i))
+                for i in range(self.parallel)
+            ]
 
-            if (n + 1) % self.eval_interval == 0:
-                ev_random = self.evaluate_agent(vs='random')
-                ev = self.evaluate_agent(vs='initial')
-                logger.info(
-                    f"{n + 1} steps"
-                    f", {ev * 100:.2f}% vs initial"
-                    f", {ev_random * 100:.2f}% vs random"
+            for p in processes:
+                p.start()
+
+            for p in processes:
+                p.join()
+
+        else:
+            self.target()
+
+    def target(self, q_table=None, lock=None, pid=0):
+        if self.parallel:
+            assert lock is not None
+            assert q_table is not None
+
+        if self.parallel:
+            self.evaluator.use_shared_dict(q_table)
+
+        n_cut = 50
+        if self.n_steps < n_cut:
+            n_cut = 1
+
+        for n in range(self.n_steps // n_cut):
+
+            state_histories = []
+            for i in range(n_cut):
+                game = Game(
+                    n_pawns=self.n_pawns,
+                    agent_0=self.agent,
+                    agent_1=self.agent,
+                    save_states=True,
                 )
-                self.evaluator.dump()
-                if ev > .9:
-                    self.evaluator.dump(self.evaluator_old.model_path)
+                game.run()
+                state_histories.append(game.state_history.copy())
+
+                step = n * n_cut + i + 1
+                if step % self.eval_interval == 0 and pid == 0:
+                    ev_random = self.evaluate_agent(vs='random')
+                    ev = self.evaluate_agent(vs='initial')
+                    logger.info(
+                        f"{step} steps"
+                        f", {ev * 100:.2f}% vs initial"
+                        f", {ev_random * 100:.2f}% vs random"
+                    )
+                    logger.info(f'{pid}, dump start')
+                    self.evaluator.dump()
+                    logger.info(f'{pid}, dump end')
+                    if ev > .9:
+                        with lock if self.parallel else nullcontext():
+                            self.evaluator.dump(self.evaluator_old.model_path)
+
+            with lock if self.parallel else nullcontext():
+                logger.info(f'{pid}, back_propagate, {len(self.evaluator.Q)}')
+                for state_history in state_histories:
+                    self.back_propagate(state_history)
+
 
     def back_propagate(self, state_history: list[State]) -> None:
         """
