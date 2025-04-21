4e42f54c413d471cb8e4c922fae95d0efc2f71f7
MartinBraquet <martin.braquet@gmail.com>
2025-04-21 19:05:05 +0700

    Astype float

--- squadro/tools/probabilities.py
+++ squadro/tools/probabilities.py
@@ -7,20 +7,23 @@ def set_seed(seed=0):
     random.seed(seed)
     np.random.seed(seed)
 
-def get_random_sample(probs: np.ndarray):
-    """
-    Get a random sample from a probability distribution
 
-    >>> get_random_sample(np.array([1, 0]))
+def get_random_index(probs: np.ndarray):
+    """
+    Get a random index from a probability distribution
+
+    >>> get_random_index(np.array([1, 0]))
     np.int64(0)
 
-    >>> get_random_sample(np.array([0, 1]))
+    >>> get_random_index(np.array([0, 1]))
     np.int64(1)
 
     >>> np.random.seed(0)
-    >>> get_random_sample(np.array([.01] * 100))
+    >>> get_random_index(np.array([.01] * 100))
     np.int64(52)
     """
+    probs = probs.astype(np.float64)
+    probs /= np.sum(probs)
     samples = np.random.multinomial(1, probs)
     index = np.where(samples == 1)[0][0]
     return index
