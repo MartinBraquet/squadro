121c0e58bcd1d83ed285f0f6b20daad052b3f9b4
MartinBraquet <martin.braquet@gmail.com>
2025-04-22 11:26:03 +0700

    Outsource get_action_from_click

--- squadro/agents/human_agent.py
+++ squadro/agents/human_agent.py
@@ -4,6 +4,18 @@ from squadro.agents.agent import Agent
 from squadro.animation.board import get_position_from_click, check_quit
 
 
+def get_action_from_click(state) -> int | None:
+    click_pos = get_position_from_click()
+    player = state.get_cur_player()
+    pawn = click_pos[player] - 1
+    if 0 <= pawn <= state.n_pawns - 1:
+        pawn_pos = state.get_pawn_position(player, pawn)
+        if pawn_pos == click_pos and state.is_action_valid(pawn):
+            return pawn
+        return None
+    return None
+
+
 class HumanAgent(Agent):
 
     def get_action(self, state, last_action=None, time_left=None):
@@ -11,14 +23,9 @@ class HumanAgent(Agent):
             for event in pygame.event.get():
                 check_quit(event)
                 if event.type == pygame.MOUSEBUTTONUP:
-                    click_pos = get_position_from_click()
-                    player = state.get_cur_player()
-                    pawn = click_pos[player] - 1
-                    if 0 <= pawn <= state.n_pawns - 1:
-                        pawn_pos = state.get_pawn_position(player, pawn)
-                        if pawn_pos == click_pos:
-                            if state.is_action_valid(pawn):
-                                return pawn
+                    pawn = get_action_from_click(state)
+                    if pawn is not None:
+                        return pawn
 
     @classmethod
     def get_name(cls):
