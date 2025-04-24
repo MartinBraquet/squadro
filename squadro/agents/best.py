590dccf70bc6e671b90fe58f998de7f14c07c611
MartinBraquet <martin.braquet@gmail.com>
2025-04-24 18:00:23 +0700

    Add MonteCarloQLearningAgent to best loggers

--- squadro/agents/best.py
+++ squadro/agents/best.py
@@ -1,9 +1,14 @@
 from squadro.agents.alphabeta_agent import AlphaBetaAdvancementDeepAgent
+from squadro.agents.montecarlo_agent import MonteCarloQLearningAgent
 from squadro.tools.constants import DefaultParams
 
 
 def get_best_agent(**kwargs):
-    return AlphaBetaAdvancementDeepAgent(**kwargs)
+    n_pawns = kwargs.pop('n_pawns', DefaultParams.n_pawns)
+    if n_pawns <= 3:
+        return MonteCarloQLearningAgent(**kwargs)
+    else:
+        return AlphaBetaAdvancementDeepAgent(**kwargs)
 
 
 def get_best_real_time_game_agent(**kwargs):
