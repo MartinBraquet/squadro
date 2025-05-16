983e4fa2233fdf33c44c13876c9be4580844fd68
MartinBraquet <martin.braquet@gmail.com>
2025-05-16 17:05:40 +0530

    Add init

--- squadro/__init__.py
+++ squadro/__init__.py
@@ -4,6 +4,7 @@ from squadro.animation.animated_game import RealTimeAnimatedGame, GameAnimation
 from squadro.game import Game
 from squadro.tools.agents import AVAILABLE_AGENTS
 from squadro.tools.log import logger
+from squadro.training.deep_q_learning import DeepQLearningTrainer
 from squadro.training.q_learning import QLearningTrainer
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
