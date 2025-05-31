307f7b4d88f5af49051b116b7555f32ac477ddf0
MartinBraquet <martin.braquet@gmail.com>
2025-05-31 22:23:33 +0530

    Refactor DeepQLearningTrainer to simplify logic, introduce ReplayBuffer, and improve backpropagation and evaluation handling

--- squadro/training/deep_q_learning.py
+++ squadro/training/deep_q_learning.py
@@ -1,19 +1,19 @@
 import json
 import random
-from contextlib import nullcontext
-from multiprocessing import Process, Manager
+import shutil
 from multiprocessing.managers import ArrayProxy  # noqa
 from pathlib import Path
 
+import numpy as np
 import torch
-from torch import nn, optim
+from torch import nn, optim, Tensor
 
 from squadro import Game
 from squadro.agents.agent import Agent
 from squadro.agents.alphabeta_agent import AlphaBetaAdvancementDeepAgent
 from squadro.agents.montecarlo_agent import MonteCarloDeepQLearningAgent
 from squadro.evaluators.evaluator import DeepQLearningEvaluator
-from squadro.tools.constants import DefaultParams, inf
+from squadro.tools.constants import DefaultParams, inf, DATA_PATH
 from squadro.tools.log import training_logger as logger
 
 
@@ -29,10 +29,10 @@ class DeepQLearningTrainer:
         lr=None,
         n_steps=None,
         backprop_interval=None,
+        backprop_steps=None,
         eval_interval=None,
         eval_steps=None,
         model_path=None,
-        parallel=None,
         max_mcts_steps=None,
         init_from=None,
     ):
@@ -43,19 +43,18 @@ class DeepQLearningTrainer:
         :param eval_interval: interval at which to evaluate the agent.
         :param eval_steps: number of steps to evaluate the agent.
         :param model_path: path to save the model.
-        :param parallel: whether to run in parallel on multiple CPUs/GPUs.
         """
         self.n_pawns = n_pawns or DefaultParams.n_pawns
         assert 2 <= self.n_pawns <= 4, "n_pawns must be between 2 and 4"
         self.n_steps = n_steps or int(5e4)
-        self.eval_interval = eval_interval or int(100 if parallel else 500)
+        self.eval_interval = eval_interval or int(500)
         self.eval_steps = eval_steps or int(100)
-        self.parallel = parallel if parallel is not None else False
         self.mcts_kwargs = dict(
             max_steps=max_mcts_steps or 20,
             max_time_per_move=inf,
         )
 
+        self.backprop_steps = backprop_steps or 50
         self.backprop_interval = backprop_interval or 100
         if self.n_steps < self.backprop_interval:
             self.backprop_interval = 1
@@ -71,6 +70,7 @@ class DeepQLearningTrainer:
             self.evaluator.erase(self.n_pawns)
 
         self.optimizer = optim.Adam(params=self.model.parameters(), lr=lr or 1e-3)
+        self.replay_buffer = ReplayBuffer(n_pawns=self.n_pawns)
 
     @property
     def model_path(self):
@@ -92,72 +92,48 @@ class DeepQLearningTrainer:
             f"Starting training: {self.n_pawns} pawns\n"
             f"lr: {self.lr}\n"
             f"{self.n_steps} steps\n"
-            f"backprop_interval: {self.backprop_interval}\n"
-            f"eval_interval: {self.eval_interval}\n"
-            f"eval_steps: {self.eval_steps}\n"
-            f"path: {self.model_path}"
+            f"backprop interval: {self.backprop_interval}\n"
+            f"backprop steps: {self.backprop_steps}\n"
+            f"eval interval: {self.eval_interval}\n"
+            f"eval steps: {self.eval_steps}\n"
+            f"path: {self.model_path}\n"
+            f"replay buffer: {self.replay_buffer}"
         )
         self.evaluator.get_model(n_pawns=self.n_pawns)
         self.evaluator.dump(self.evaluator_old.model_path)
         self.evaluator_old.clear()
 
-        if self.parallel:
-            manager = Manager()
-            q_shared = manager.dict()
-            lock = manager.Lock()
+        self._run()
 
-            processes = [
-                Process(target=self._run, args=(q_shared, lock, i))
-                for i in range(self.parallel)
-            ]
-
-            for p in processes:
-                p.start()
-
-            for p in processes:
-                p.join()
-
-        else:
-            self._run()
-
-    def _run(self, q_shared=None, lock=None, pid=0):
-        if self.parallel:
-            assert lock is not None
-            assert q_shared is not None
-            self.evaluator.set_model(q_shared, n_pawns=self.n_pawns)
-
-        replay_buffer = []
+    def _run(self):
         results_eval = {}
-        for n in range(self.n_steps // self.backprop_interval):
+        for step in range(1, self.n_steps + 1):
+            print(step)
+            self.get_training_samples()
 
-            for i in range(self.backprop_interval):
-                replay_buffer += self.get_training_samples()
-                step = n * self.backprop_interval + i + 1
-                print(step)
-                if step % self.eval_interval == 0 and pid == 0:
-                    ev = self.evaluate_agent(vs='initial')
-                    ev_random = self.evaluate_agent(vs='random')
-                    ev_other = self.evaluate_agent(vs=AlphaBetaAdvancementDeepAgent(max_depth=5))
-                    logger.info(
-                        f"{step} steps"
-                        f", {ev * 100 :.0f}% vs initial"
-                        f", {ev_other * 100 :.0f}% vs ab_deep"
-                        f", {ev_random * 100 :.0f}% vs random"
-                    )
-                    results_eval[step] = dict(
-                        ev=ev,
-                        ev_random=ev_random,
-                        ev_ab_deep=ev_other,
-                    )
-                    logger.info(f'{pid}, dump start')
+            if step % self.backprop_interval == 0:
+                with logger.context_info('back_propagate'):
+                    self.back_propagate()
+                self.replay_buffer.save()
+
+            if step % self.eval_interval == 0:
+                ev = self.evaluate_agent(vs='initial')
+                ev_random = self.evaluate_agent(vs='random')
+                ev_other = self.evaluate_agent(vs=AlphaBetaAdvancementDeepAgent(max_depth=5))
+                logger.info(
+                    f"{step} steps"
+                    f", {ev * 100 :.0f}% vs initial"
+                    f", {ev_other * 100 :.0f}% vs ab_deep"
+                    f", {ev_random * 100 :.0f}% vs random"
+                )
+                results_eval[step] = dict(
+                    ev=ev,
+                    ev_random=ev_random,
+                    ev_ab_deep=ev_other,
+                )
+                with logger.context_info('dump'):
                     json.dump(results_eval, open(f'results_eval.json', 'w'))
                     self.evaluator.dump()
-                    logger.info(f'{pid}, dump end')
-
-            with lock if self.parallel else nullcontext():
-                logger.info(f'{pid}, back_propagate start')
-                self.back_propagate(replay_buffer)
-                logger.info(f'{pid}, back_propagate end')
 
         self.evaluator.dump()
 
@@ -172,38 +148,46 @@ class DeepQLearningTrainer:
 
         history = []
         for i, s in enumerate(game.state_history):
+            cur_player = s.cur_player
+            s = s.to_list()
+
+            move_probs = game.move_info[i]['mcts_move_probs'] if i < len(game.move_info) else None
+            check_nan(move_probs)
+            if isinstance(move_probs, np.ndarray):
+                move_probs = move_probs.tolist()
+
             history.append((
                 s,
-                game.move_info[i]['mcts_move_probs'] if i < len(game.move_info) else None,
-                1 if game.winner == s.cur_player else -1,
+                move_probs,
+                1 if game.winner == cur_player else -1,
             ))
+
+        self.replay_buffer += history
 
         return history
 
-    def back_propagate(self, replay_buffer: list) -> None:
+    def back_propagate(self) -> None:
         """
         Update the Q-network based on the reward obtained at the end of the game.
 
-        :param replay_buffer: List of training samples.
         :return: None
         """
         self.model.train()
         v_loss = nn.MSELoss()
         p_loss = nn.CrossEntropyLoss()
-        batches = random.sample(replay_buffer, self.backprop_interval * 10)
+        batches = random.sample(self.replay_buffer.buffer, k=self.backprop_steps)
 
         for state, probs, reward in batches:
-            assert reward in {-1, 1}
+            assert reward in {-1, 1}, f"Got reward {reward} instead of {-1, 1} for state {state}"
             reward = torch.FloatTensor([reward])
             self.optimizer.zero_grad()
-            p, v = self.evaluator.evaluate(state, is_torch=True, check_game_over=False)
+            p, v = self.evaluator.evaluate(state, torch_output=True, check_game_over=False)
             check_nan(p)
             check_nan(v)
             check_nan(reward)
             loss = v_loss(reward, v)
             if probs is not None:
-                probs = torch.from_numpy(probs)
-                check_nan(probs)
+                probs = torch.FloatTensor(probs)
                 loss += p_loss(p, probs)
             check_nan(loss)
             loss.backward()
@@ -239,13 +223,62 @@ class DeepQLearningTrainer:
         return v / self.eval_steps
 
 
-def check_nan(tensor):
+def check_nan(data):
     """
     >>> check_nan(torch.Tensor([1, 2, 3]))
+    >>> check_nan(np.array([1, 2, 3]))
+    >>> check_nan(None)
     >>> check_nan(torch.Tensor([1, float('nan'), 3]))
     Traceback (most recent call last):
        ...
-    RuntimeError: NaN detected in network output: tensor([1., nan, 3.])
+    RuntimeError: NaN detected in: tensor([1., nan, 3.])
+    >>> check_nan(np.array([1, float('nan'), 3]))
+    Traceback (most recent call last):
+       ...
+    RuntimeError: NaN detected in: [ 1. nan  3.]
     """
-    if torch.isnan(tensor).any():
-        raise RuntimeError(f"NaN detected in network output: {tensor}")
+    if data is None:
+        return
+    if isinstance(data, Tensor) and not torch.isnan(data).any():
+        return
+    if isinstance(data, np.ndarray) and not np.isnan(data).any():
+        return
+    raise RuntimeError(f"NaN detected in: {data}")
+
+
+class ReplayBuffer:
+    """
+    Replay buffer stored in disk
+    """
+
+    def __init__(self, path: Path = None, n_pawns: int = None):
+        if n_pawns:
+            path = DATA_PATH / "deep_q_learning" / f"replay_buffer_{n_pawns}.json"
+        if path is None:
+            raise ValueError("path must be specified")
+        self.path = Path(path)
+        self.buffer = []
+        self.load()
+
+    def __repr__(self):
+        return f"{len(self.buffer)} samples @ {self.path}"
+
+    def load(self):
+        if self.path.exists():
+            self.buffer = json.load(open(self.path, 'r'))
+
+    def append(self, sample):
+        self.buffer.append(sample)
+
+    def __add__(self, other):
+        self.buffer += other
+        return self
+
+    def save(self):
+        shutil.copy(self.path, self.path.with_suffix('.bak'))
+        json.dump(self.buffer, open(self.path, 'w'))
+
+    def pretty_save(self):
+        text = '\n'.join(map(str, self.buffer))
+        with open(self.path.with_suffix('.txt'), 'w') as f:
+            f.write(text)
