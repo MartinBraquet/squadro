df0ea856a116b9b5fc6bc4540685990675237e10
MartinBraquet <martin.braquet@gmail.com>
2025-06-07 20:33:19 +0530

    remove warnings

--- squadro/__init__.py
+++ squadro/__init__.py
@@ -1,6 +1,7 @@
+import logging
 import os
 
-from squadro.animation.animated_game import RealTimeAnimatedGame, GameAnimation
+from squadro.animation.animated_game import GamePlay, GameAnimation
 from squadro.game import Game
 from squadro.tools.agents import AVAILABLE_AGENTS
 from squadro.tools.log import logger
@@ -8,3 +9,6 @@ from squadro.training.deep_q_learning import DeepQLearningTrainer
 from squadro.training.q_learning import QLearningTrainer
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
+
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+logging.getLogger('PIL').setLevel(logging.WARNING)
