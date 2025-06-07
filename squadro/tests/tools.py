349b50cb7f2ca7a29e083e597b508bad66a0445c
MartinBraquet <martin.braquet@gmail.com>
2025-06-07 20:45:11 +0530

    Add ML test case

---
+++ squadro/tests/tools.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from unittest import TestCase
+
+from squadro.state import State
+from squadro.tools.probabilities import set_seed
+
+
+class ML(TestCase, ABC):
+    def setUp(self):
+        set_seed()
+        self.evaluator = self.get_evaluator()
+
+    @abstractmethod
+    def get_evaluator(self):
+        """Each child class must implement this to create its specific evaluator"""
+        pass
+
+    def test_game_over(self):
+        state = State(advancement=[[8, 8, 3], [1, 2, 4]], cur_player=0)
+        value = self.evaluator.get_value(state)
+        self.assertEqual(1, value)
+
+        state.cur_player = 1
+        value = self.evaluator.get_value(state)
+        self.assertEqual(-1, value)
