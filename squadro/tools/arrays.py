7082e80eaae6ac6aa3c9cdc50c2da8f731336cd8
MartinBraquet <martin.braquet@gmail.com>
2025-06-13 00:02:44 +0530

    feat: add array formatting utility in arrays module
    - Imported NumPy for array operations
    - Added `array2string` method to format arrays as strings with custom float precision
    - Ensured formatter supports `float_kind` with two decimal places

--- squadro/tools/arrays.py
+++ squadro/tools/arrays.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def box(x):
     if isinstance(x, list):
         return x
@@ -8,3 +11,11 @@ def unbox(x):
     if isinstance(x, list) and len(x) == 1:
         x = x[0]
     return x
+
+
+def array2string(arr):
+    formatter = {'float_kind': lambda x: f"{x:.2f}"}
+    return np.array2string(
+        arr,
+        formatter=formatter  # noqa
+    )
