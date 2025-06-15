d2ad56e64dd8c006d65cfdc3005a873f27ac566b
MartinBraquet <martin.braquet@gmail.com>
2025-06-15 17:40:41 +0530

    Update

--- scripts/test_mypi.sh
+++ scripts/test_mypi.sh
@@ -2,5 +2,6 @@
 
 conda create -n test python=3.12 pip -y
 conda activate test
+pip install torch --index-url https://download.pytorch.org/whl/cpu
 pip install squadro
-python -c "from squadro import game; game.run()"
+python -c "from squadro import Game; Game().run()"
