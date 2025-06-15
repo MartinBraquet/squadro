14a6683ae3993283566dd8b5811b19840f13a983
MartinBraquet <martin.braquet@gmail.com>
2025-06-15 14:20:25 +0530

    Fix test

--- squadro/tests/base.py
+++ squadro/tests/base.py
@@ -1,3 +1,4 @@
+from numbers import Number
 from unittest import TestCase
 
 import numpy as np
@@ -8,5 +9,7 @@ class Base(TestCase):
     def assertEqualGeneral(self, first, second):
         if isinstance(first, np.ndarray):
             np.testing.assert_array_almost_equal(first, second, decimal=5)
+        elif isinstance(first, Number):
+            self.assertAlmostEqual(first, second, places=5)
         else:
             self.assertEqual(first, second)
